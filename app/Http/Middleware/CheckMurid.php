ef3fec77dd0e9388f240f539529bf699e7652108
Setyo Adi Wibowo <setyaadii12@gmail.com>
2019-02-28 00:31:01 +0700

    add middleware,role,controller,fix routes

---
+++ app/Http/Middleware/CheckMurid.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckMurid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (! $request->user()->hasRole($role)) {
+            return redirect('/guru');
+        }
+        return $next($request);
+    }
+}
