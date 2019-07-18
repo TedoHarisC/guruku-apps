9edfbc050a6896bac7fa2704617e4229dcc005fe
TedoHarisC <tedoharischandra29@gmail.com>
2019-07-19 02:28:40 +0700

    Make Dashboard murid more responsive

--- resources/views/guru/layouts/app.blade.php
+++ resources/views/guru/layouts/app.blade.php
@@ -73,9 +73,6 @@
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-                    <!-- <div class=" dropdown-header noti-title">
-                        <h6 class="text-overflow m-0">{{ __('Welcome!') }}</h6>
-                    </div> -->
                     <a href="{{ route('dashboard') }}" class="dropdown-item">
                         <i class="ni ni-app"></i>
                         <span>{{ __('Dashboard') }}</span>
